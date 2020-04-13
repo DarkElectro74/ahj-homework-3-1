565038bc4e1d744d0406b9d4e066a0b9499cc132
DarkElectro <electrodark74@gmail.com>
2020-04-13 13:30:41 +0500

    ?

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import GameBoard from './GameBoard.js';
-import GamePlay from './GamePlay.js';
-
-const gamesBoard = new GameBoard(4);
-gamesBoard.drawBoard();
-const gamePlay = new GamePlay(gamesBoard);
-gamePlay.init();
